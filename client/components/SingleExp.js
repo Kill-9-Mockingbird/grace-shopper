e869256e7a9b5b9855da86d00d67c1c7f4bf7d6e
Esther Kim <esther.ys.kim@gmail.com>
2020-03-11 01:05:01 -0400

    ui finished

--- client/components/SingleExp.js
+++ client/components/SingleExp.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import Button from '@material-ui/core/Button'
-import CardMedia from '@material-ui/core/CardMedia'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
-import {makeStyles} from '@material-ui/core/styles'
-import Container from '@material-ui/core/Container'
+import {
+  Typography,
+  Grid,
+  Button,
+  Card,
+  CardMedia,
+  makeStyles,
+  Container,
+  CssBaseline
+} from '@material-ui/core/'
+
 import {Link as RouterLink} from 'react-router-dom'
 
 const useStyles = makeStyles(theme => ({
@@ -19,9 +23,14 @@ const useStyles = makeStyles(theme => ({
   cardMedia: {
     paddingTop: '56.25%' // 16:9
   },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6)
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  description: {
+    maxWidth: '80%',
+    alignSelf: 'center'
   }
 }))
 
@@ -35,73 +44,77 @@ export const SingleExp = props => {
       <main key={experience.id}>
         <div className={classes.heroContent}>
           <Container maxWidth="sm">
-            <CardMedia
-              className={classes.cardMedia}
-              image={experience.imageUrl}
-              title={experience.id}
-            />
-            <br />
-            <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="textPrimary"
-            >
-              {experience.name}
-            </Typography>
-            <Typography
-              component="h4"
-              variant="h4"
-              align="center"
-              color="textSecondary"
-              gutterBottom
-            >
-              with {celebrity.name}
-            </Typography>
-            <Typography variant="subtitle2" align="center">
-              <strong>Location</strong>: {experience.city}, {experience.state}
+            <Card className={classes.card}>
+              <CardMedia
+                image={experience.imageUrl}
+                className={classes.cardMedia}
+              />
               <br />
-              <strong>Duration</strong>: {experience.duration}
+              <Typography
+                component="h1"
+                variant="h2"
+                align="center"
+                color="textPrimary"
+              >
+                {experience.name}
+              </Typography>
+              <Typography
+                component="h4"
+                variant="h4"
+                align="center"
+                color="textSecondary"
+                gutterBottom
+              >
+                with {celebrity.name}
+              </Typography>
+              <Typography variant="subtitle2" align="center">
+                <strong>Location</strong>: {experience.city}, {experience.state}
+                <br />
+                <strong>Duration</strong>: {experience.duration}
+                <br />
+                <strong>Group Size</strong>: {experience.groupSize}
+                <br />
+                <strong>Price</strong>: ${experience.price}
+              </Typography>
               <br />
-              <strong>Group Size</strong>: {experience.groupSize}
-              <br />
-              <strong>Price</strong>: ${experience.price}
-            </Typography>
-            <Typography
-              variant="body1"
-              align="center"
-              color="textSecondary"
-              paragraph
-            >
-              {experience.description}
-            </Typography>
-            <div className={classes.heroButtons}>
-              <Grid container spacing={2} justify="center">
-                <Grid item>
-                  <Button
-                    variant="outlined"
-                    color="primary"
-                    component={RouterLink}
-                    to="/experiences"
-                  >
-                    Go Back
-                  </Button>
+              <Typography
+                variant="body1"
+                align="center"
+                color="textSecondary"
+                className={classes.description}
+                paragraph
+              >
+                {experience.description}
+              </Typography>
+              <div className={classes.heroButtons}>
+                <Grid container spacing={2} justify="center">
+                  <Grid item>
+                    <Button
+                      variant="outlined"
+                      color="primary"
+                      component={RouterLink}
+                      to="/experiences"
+                    >
+                      Go Back
+                    </Button>
+                  </Grid>
+                  <Grid item>
+                    <Button
+                      type="button"
+                      variant="contained"
+                      color="primary"
+                      id={experience.id}
+                      onClick={() => {
+                        handleAdd(experience.id)
+                      }}
+                    >
+                      Add To Cart
+                    </Button>
+                  </Grid>
                 </Grid>
-                <Grid item>
-                  <Button
-                    type="button"
-                    variant="contained"
-                    color="primary"
-                    id={experience.id}
-                    onClick={() => {
-                      handleAdd(experience.id)
-                    }}
-                  >
-                    Add To Cart
-                  </Button>
-                </Grid>
-              </Grid>
-            </div>
+              </div>
+              <br />
+            </Card>
           </Container>
         </div>
       </main>
--- client/components/cartItems.js
+++ client/components/cartItems.js
@@ -1,55 +1,108 @@
 import React from 'react'
+import {
+  Button,
+  makeStyles,
+  Typography,
+  Card,
+  CardMedia,
+  CardContent
+} from '@material-ui/core'
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex'
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: 200,
+    // height: 200,
+    paddingLeft: 30
+  },
+  content: {
+    flex: '1 0 auto'
+  },
+  cover: {
+    width: 200
+    // height: 200
+  },
+  card: {
+    height: '100%',
+    maxWidth: 600,
+    display: 'flex',
+    flexDirection: 'row'
+  }
+}))
 
 const CartItems = props => {
   const {experience, handleRemove, increase, decrease, orderId} = props
 
   return experience.name ? (
-    <div className="cartItems">
-      <img className="thumbnail" src={experience.imageUrl} />
-      <p>Name: {experience.name}</p>
-      <p>Hosted By: {experience.celebrity.name}</p>
-      <p>Price: ${experience.price}</p>
-      <div className="quantity">
-        {/* if qty is 1, clicking - will remove item from the cart
-        otherwise, it will just decrement*/}
-        {experience.orderDetail.packageQty <= 1 ? (
-          <i
-            className="fas fa-minus"
-            onClick={() => handleRemove(`${experience.id}`, event)}
-          />
-        ) : (
-          <i
-            className="fas fa-minus"
-            onClick={() => {
-              decrease(orderId, experience.id)
-            }}
-          />
-        )}
-        Quantity: {experience.orderDetail.packageQty}
-        <i
-          className="fas fa-plus"
-          onClick={() => {
-            increase(orderId, experience.id)
-          }}
-        />
+    <Card key={experience.id} className={useStyles().card}>
+      <CardMedia
+        image={experience.imageUrl}
+        title={experience.name}
+        className={useStyles().cover}
+      />
+      <div>
+        <CardContent className={useStyles().details}>
+          <Typography variant="h6">
+            <strong>{experience.name}</strong>
+          </Typography>
+          <Typography variant="subtitle2">
+            <em>with {experience.celebrity.name}</em>
+          </Typography>
+          <Typography variant="subtitle2" color="textSecondary">
+            ${experience.price}
+          </Typography>
+          <br />
+          <div className="quantity">
+            {experience.orderDetail.packageQty <= 1 ? (
+              <i
+                className="fas fa-minus"
+                onClick={() => handleRemove(`${experience.id}`, event)}
+              />
+            ) : (
+              <i
+                className="fas fa-minus"
+                onClick={() => {
+                  decrease(orderId, experience.id)
+                }}
+              />
+            )}
+            <Typography variant="subtitle2">
+              Quantity: {experience.orderDetail.packageQty}
+            </Typography>
+            <i
+              className="fas fa-plus"
+              onClick={() => {
+                increase(orderId, experience.id)
+              }}
+            />
+          </div>
+          <br />
+          <Typography variant="subtitle2">
+            <strong>Total Price</strong>: $
+            {experience.price * experience.orderDetail.packageQty}
+          </Typography>
+
+          <div className={useStyles().button}>
+            <Button
+              size="small"
+              variant="text"
+              color="secondary"
+              onClick={event => {
+                handleRemove(`${experience.id}`, event)
+              }}
+            >
+              Delete Item
+            </Button>
+          </div>
+        </CardContent>
       </div>
-      <br />
-      <p>
-        Total Price: ${experience.price * experience.orderDetail.packageQty}
-      </p>
-      <br />
-      <br />
-      <button
-        type="button"
-        onClick={event => {
-          handleRemove(`${experience.id}`, event)
-        }}
-      >
-        Delete Item
-      </button>
-    </div>
+    </Card>
   ) : (
-    <div>Loading...</div>
+    <Typography variant="subtitle2">Loading...</Typography>
   )
 }
 
